258de59a6d09ea26dc7521bffcd74cc5e1ea5d36
kapilsinghrajput <kplsinghshekhawat@gmail.com>
2024-04-12 22:08:57 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
-const connectToMongo = async()=>{
-const res = await mongoose.connect("mongodb+srv://kplsinghshekhawat:kapil12345@cluster0.m5zfg7x.mongodb.net/myusers",{
-    family:4
-})
-if(res){
-    console.log("db connected");
-}
-}
+mongoose.connect( `mongodb+srv://kplsinghshekhawat:kapil12345@cluster0.m5zfg7x.mongodb.net/myusers`, {
+    useNewUrlParser: true,   
+    serverSelectionTimeoutMS: 5000,    
+    autoIndex: false, // Don't build indexes 
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6 
+ }).then(() => {
+   console.log('MongoDB connected successfully');
+ }).catch((err) => {
+   console.error('MongoDB connection error: ', err);
+ }); 
+
 
 module.exports = connectToMongo
