d740707b304aa0bd5b5adf8511e25452068ea820
kapilsinghrajput <kplsinghshekhawat@gmail.com>
2024-04-12 16:58:01 +0530

    first commit

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
   res.header(
-  "Access-Control-Allow-Origin",
-  "http://localhost:3000"
+  {"Access-Control-Allow-Origin":"http://localhost:3000"}
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
